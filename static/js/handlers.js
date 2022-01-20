4b9cd27054670debb4b1c5ac98439ebab8589d4c
rwclutterbuck <rwclutterbuck@outlook.com>
2022-01-20 10:55:53 +0000

    greeting JS working

--- static/js/handlers.js
+++ static/js/handlers.js
@@ -1,12 +1,11 @@
 function createGreeting(first_name, last_name, event, message) {
-    console.log("Greet the unicorns")
     let greeting = document.querySelector("#greeting-title");
     let outputMessage = document.querySelector("#greeting-message");
     let outputEvent = document.querySelector("#greeting-card");
 
-    greeting.innerHTML = "Unicorns";
-    outputMessage.innerHTML = "Hello unicorns";
-    outputEvent.background = "red";
+    greeting.innerHTML = `${first_name.value} ${last_name.value}`;
+    outputMessage.innerHTML = message.value;
+    outputEvent.backgroundImage = "https://learnenglishteens.britishcouncil.org/sites/teens/files/styles/article/public/field/image/rs185_177502406-low.jpg?itok=ubWa1SJj";
 }
 
 module.exports = {
