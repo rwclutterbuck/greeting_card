9f36f8434673a3740c7aae2a8c2400e78551fcf0
bojin11 <15011278723@163.com>
2022-01-20 17:05:02 +0000

    change background

--- static/js/index.js
+++ static/js/index.js
@@ -1,14 +1,39 @@
-const handlers = require('./handlers');
+// const handlers = require('./handlers');
+
+function showBackground(event){
+    if (event=="Birthday"){
+        document.body.style.backgroundImage = "url(../img/Birthday.jpg)";
+        document.querySelector("#greeting-title").textContent = "Happy Birthday!"
+    } 
+    if (event=="New Year"){
+        document.body.style.backgroundImage = "url(../img/new-year.jpg)";
+        document.querySelector("#greeting-title").textContent = "Happy New Year!"
+    }
+    if (event=="Christmas"){
+        document.body.style.backgroundImage = "url(../img/Christmas.jpg)";
+        document.querySelector("#greeting-title").textContent = "Merry Christmas!"
+    }
+
+}
 
 function init() {
-    const greetingForm = document.querySelector('#');
+    const greetingForm = document.querySelector('form');
     greetingForm.addEventListener("submit", e => {
         console.log("Leprecorn event")
         e.preventDefault();
         const first_name = e.target.fname;
         const last_name = e.target.lname;
-        const event = e.target.event;
+        const event = e.target.event.value;
         const message = e.target.message;
-        handlers.createGreeting(fname, lname, event, message);
+        //handlers.createGreeting(fname, lname, event, message);
+        showBackground(event);
     })
 }
+
+
+
+
+init();
+
+
+// module.exports = {showBackground, init};
